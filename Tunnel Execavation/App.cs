7022822066a95e09e965bb4ef207e6964302b931
ruohan.wang <ruohan.wang@USSF1LJBFV0F3>
2022-06-18 17:11:15 -0600

    Add project files.

---
+++ Tunnel Execavation/App.cs
@@ -0,0 +1,106 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Reflection;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+#endregion
+
+namespace Tunnel_Execavation
+{
+    internal class App : IExternalApplication
+    {
+        const string RIBBON_TAB = "ruohan test tools";
+        const string RIBBON_PANEL = "Tunnel Excavation";
+        public Result OnStartup(UIControlledApplication a)
+        {
+            // get the RIBBON_TAB 
+            try
+            {
+                a.CreateRibbonTab(RIBBON_TAB);
+            }
+            catch (Exception) { } // the exception catched when the tab already exists
+
+            // get or create RIBBON_PANEL and add it to the RIBBON_TAB
+            // get the RIBBON_PANEL if it exists
+            RibbonPanel panel = null;
+            List<RibbonPanel> panels = a.GetRibbonPanels(RIBBON_TAB);
+            foreach (RibbonPanel pnl in panels)
+            {
+                if (pnl.Name == RIBBON_PANEL)
+                {
+                    panel = pnl;
+                    break;
+                }
+            }
+            // create RIBBON_PANEL if it doesn't exist
+            if (panel == null)
+            {
+                panel = a.CreateRibbonPanel(RIBBON_TAB, RIBBON_PANEL);
+            }
+
+            // get the image for the button - make sure the image is 32 x 32 pixels and 69 dpi 
+            Image img = Properties.Resources.sample_icon_32x32;
+            ImageSource imgSrc = GetBitmapSource(img);
+
+            // create the button data
+            PushButtonData btnData = new PushButtonData(
+                "Tunnel",
+                "Create Tunnel",
+                Assembly.GetExecutingAssembly().Location,
+                "Tunnel_Execavation.Command"
+                )
+            {
+                ToolTip = "short description that is shown when you hover over the button",
+                LongDescription = "longer desciption shown when you hover over the button for a few seconds",
+                Image = imgSrc,
+                LargeImage = imgSrc
+            };
+
+            // add the button to the ribbon 
+            PushButton button = panel.AddItem(btnData) as PushButton;
+            button.Enabled = true;
+
+            //for a detail list for all properties of the panel and button, search RibbonPanel Class in Revit API.
+
+            return Result.Succeeded;
+        }
+
+
+        public Result OnShutdown(UIControlledApplication a)
+        {
+            return Result.Succeeded;
+        }
+
+        private BitmapSource GetBitmapSource(Image img)
+        {
+            BitmapImage bmp = new BitmapImage();
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                img.Save(ms, ImageFormat.Png);
+                ms.Position = 0;
+
+                bmp.BeginInit();
+
+                bmp.CacheOption = BitmapCacheOption.OnLoad;
+                bmp.UriSource = null;
+                bmp.StreamSource = ms;
+
+                bmp.EndInit();
+            }
+
+            return bmp;
+        }
+    }
+}
---
+++ Tunnel Execavation/Command.cs
@@ -0,0 +1,154 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+#endregion
+
+namespace Tunnel_Execavation
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            bool isAFamilyDoc = CheckFamily(doc);
+
+            if (!isAFamilyDoc)
+            {
+                CreateFamily(app);
+            }
+            // tell Revit it is succeeded
+            return Result.Succeeded;
+        }
+
+        // Create new swept blend family
+        // check whether this is a family file
+        private bool CheckFamily(Document doc)
+        {
+
+            if (!doc.IsFamilyDocument)
+            {
+                return false;
+            }
+            else
+            {
+                TaskDialog td = new TaskDialog("Sucess")
+                {
+                    Title = "Success 001",
+                    AllowCancellation = true,
+                    MainInstruction = "success",
+                    MainContent = "we are in a family file."
+                };
+
+                td.CommonButtons = TaskDialogCommonButtons.Ok;
+                td.Show();
+
+                return true;
+            }
+        }
+
+        // create and a new family 
+        public static Document CreateFamily(Application app)
+        {
+            Document familyDocument = null;
+
+            // get the path of family template folder
+            string famTemplatePath = app.FamilyTemplatePath;
+            string[] paths = { famTemplatePath, @"English\Metric Generic Model.rft" };
+            string familyTemplateFolderPath = Path.Combine(paths);
+
+            // create generic family
+            try
+            {
+                familyDocument = app.NewFamilyDocument(familyTemplateFolderPath);
+                familyDocument.SaveAs(@"c:\temp\TestCreationInFamily.rfa");
+
+                TaskDialog td = new TaskDialog("Sucess")
+                {
+                    Title = "Success 002",
+                    AllowCancellation = true,
+                    MainInstruction = "success",
+                    MainContent = "create family - TestCreationInFamily with generic family template"
+                };
+
+                td.CommonButtons = TaskDialogCommonButtons.Ok;
+                td.Show();
+
+                return familyDocument;
+            }
+            catch (Exception e)
+            {
+                TaskDialog td = new TaskDialog("Error")
+                {
+                    Title = "Error 001",
+                    AllowCancellation = true,
+                    MainInstruction = "Cannot Create family",
+                    MainContent = $"{e.Message}: \n {familyTemplateFolderPath}"
+                };
+
+                td.CommonButtons = TaskDialogCommonButtons.Ok;
+                td.Show();
+
+                return familyDocument;
+            }
+        }
+
+        // load family to the Reivt file
+        // check whether the family already loaded
+        public static Element FindElementByName(Document doc, Type targetType, string targetName)
+        {
+            return new FilteredElementCollector(doc)
+                .OfClass(targetType)
+                .FirstOrDefault<Element>(
+                 e => e.Name.Equals(targetName)
+                );
+        }
+
+        public static void Loadfamily(Element loadedFamily, string FamilyPath, string FamilyName)
+        {
+            if (null == loadedFamily)
+            {
+                if (!File.Exists(FamilyPath))
+                {
+                    TaskDialog td = new TaskDialog("Error")
+                    {
+                        Title = "Error 002",
+                        AllowCancellation = true,
+                        MainInstruction = "Cannot Load Family",
+                        MainContent = $"Please ensure that the family file {FamilyName} exists in {FamilyPath}"
+                    };
+
+                    td.CommonButtons = TaskDialogCommonButtons.Ok;
+                    td.Show();
+                }
+            }
+        }
+
+        // edit family
+        // create 1st sweepProfile of the tunnel
+        // create 2nd sweepProfile of the tunnel
+        // create the reference path
+        // rotate the sweepProfile to be prependiculate to the reference path
+
+        
+        // Delete family file from h
+
+    }
+
+}
